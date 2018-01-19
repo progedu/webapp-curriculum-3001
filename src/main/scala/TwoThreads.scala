6b6cb5372bbdaf6ab7fc5ecb58648d4517ac1d42
tab01105 <tab01105@gmail.com>
2018-01-19 15:51:12 +0900

    TwoThreadsの実装

--- src/main/scala/TwoThreads.scala
+++ src/main/scala/TwoThreads.scala
@@ -1,8 +1,26 @@
 object TwoThreads extends App {
   var now: Long = 0L
 
-  val threadA = new Thread(() => ??? )
-  val threadB = new Thread(() => ??? )
+  val threadA = new Thread(() => {
+    Thread.sleep(1000)
+    synchronized {
+      now = System.currentTimeMillis
+    }
+  })
+
+  val threadB = new Thread(() => {
+    val last = now
+    var changed = false
+    while (!changed) {
+      Thread.sleep(100)
+      synchronized {
+        if (last != now) {
+          println("now changed :" + now)
+          changed = true
+        }
+      }
+    }
+  })
 
   threadA.start()
   threadB.start()
