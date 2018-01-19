e10f7aac1b9d1ea889d7ef9a8cc3c660b2b89a05
tab01105 <tab01105@gmail.com>
2018-01-19 15:30:11 +0900

    TenThousandNamePrinterの実装

--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,4 +1,9 @@
 object TenThousandNamePrinter extends App {
 
-
+  for (i <- 1 to 10000) {
+    new Thread(() => {
+      Thread.sleep(1000)
+      println(Thread.currentThread().getName)
+    }).start()
+  }
 }
