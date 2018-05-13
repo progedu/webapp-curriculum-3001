32c3366547584504554eea505dacc2c6611264a8
haru221 <gadget.tf@gmail.com>
2018-05-13 14:33:06 +0900

    スレッドの挑戦問題の解答

--- src/main/scala/QuadNumberPrinter.scala
+++ src/main/scala/QuadNumberPrinter.scala
@@ -1,3 +1,11 @@
 object QuadNumberPrinter extends App {
+  def countUp(threadNo: Int): Unit = {
+    for (i <- 1 to 100000) {
+      println(s"thread ${threadNo}: ${i}")
+    }
+  }
 
+  for (i <- 0 to 4) {
+    new Thread(() => {countUp(i)}).start()
+  }
 }
