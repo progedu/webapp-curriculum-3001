6d43ce555749a4aeb2253405ad9750acd002d69f
BEN2suzuka <isobe.naoki.suzuka@gmail.com>
2019-06-15 16:05:59 +0900

    初級・中級・上級

--- build.sbt
+++ build.sbt
@@ -3,4 +3,3 @@ name := "webapp-curriculum-3001"
 version := "1.0"
 
 scalaVersion := "2.12.7"
-        
--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,3 +1,11 @@
 object TenThousandNamePrinter extends App {
 
+  for (i <- 1 to 10000) {
+    val thread = new Thread(() => {
+      Thread.sleep(1000)
+      println(Thread.currentThread().getName)
+    })
+    thread.start()
+  }
+
 }
