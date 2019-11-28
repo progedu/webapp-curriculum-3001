39ae09f05df308e1df6b8b028d732e5946c0274d
Yosuke Ogata <ogata0227@outlook.com>
2019-11-28 13:27:40 +0900

    初級解答

--- src/main/scala/QuadNumberPrinter.scala
+++ src/main/scala/QuadNumberPrinter.scala
@@ -1,3 +1,14 @@
 object QuadNumberPrinter extends App {
+  var counter = 0
 
+  def next:Int = synchronized {
+    counter = counter + 1
+    counter
+  }
+
+  for(i <- 1 to 5){
+    new Thread(() => for(j <- 1 to 20000){
+      println(Thread.currentThread().getName() +": " + next)
+    }).start()
+  }
 }
