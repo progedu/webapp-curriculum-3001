9672ba8196fcf2082616019b1f8109ea81a55c91
QWYNG <ikusawasi@gmail.com>
2020-03-21 12:48:57 +0900

    imple QuadNumberPrinter

--- src/main/scala/QuadNumberPrinter.scala
+++ src/main/scala/QuadNumberPrinter.scala
@@ -1,3 +1,14 @@
 object QuadNumberPrinter extends App {
 
+  def count(i: Int): Int = {
+    println(s"${Thread.currentThread.getName} : ${i}")
+    if (i == 100000) i
+    else
+      count(i + 1)
+  }
+
+  for (i <- 1 to 4) {
+    new Thread(() => count(1)).start()
+  }
+
 }
