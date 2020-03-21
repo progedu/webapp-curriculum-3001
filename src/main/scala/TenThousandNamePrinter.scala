c7ed4567c0611b82ff803b886bf53c64260a2bc7
QWYNG <ikusawasi@gmail.com>
2020-03-21 13:18:08 +0900

    imple TenThousandNamePrinter

--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,3 +1,10 @@
 object TenThousandNamePrinter extends App {
 
+  for (i <- 0 to 100000) {
+    new Thread(() => {
+      Thread.sleep(1000)
+      println(Thread.currentThread().getName())
+    }).start()
+  }
+
 }
