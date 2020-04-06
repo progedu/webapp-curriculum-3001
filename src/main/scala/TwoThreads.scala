f670d93e77e4b7e2682166df6a170c270fc2b299
Yohei Toriyama <kagemomiji@gmail.com>
2020-04-06 11:49:35 +0900

    上級実装

--- src/main/scala/TwoThreads.scala
+++ src/main/scala/TwoThreads.scala
@@ -1,8 +1,19 @@
 object TwoThreads extends App {
   var now: Long = 0L
 
-  val threadA = new Thread(() => ??? )
-  val threadB = new Thread(() => ??? )
+  val threadA = new Thread(() => {
+    println("start threadA")
+    Thread.sleep(1000)
+    now = System.currentTimeMillis()
+    println("end threadA")
+  })
+
+  val threadB = new Thread(() =>{
+    println("start threadB")
+    threadA.join()
+    println(now.toString())
+    println("end threadB")
+  })
 
   threadA.start()
   threadB.start()
