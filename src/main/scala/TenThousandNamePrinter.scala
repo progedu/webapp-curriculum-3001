ce85797784d5520407d1c55831feb05c7ff5b4b9
Yohei Toriyama <kagemomiji@gmail.com>
2020-04-06 11:35:25 +0900

    中級実装

--- src/main/scala/TenThousandNamePrinter.scala
+++ src/main/scala/TenThousandNamePrinter.scala
@@ -1,3 +1,15 @@
 object TenThousandNamePrinter extends App {
 
+    println(s"start ${Thread.currentThread.getName}")
+
+    def printName: Unit = {
+        Thread.sleep(1000)
+        println(s"${Thread.currentThread.getName()}")
+    }
+
+    for (i <- 1 to 10000){
+        new Thread(()=>printName).start()
+    }
+
+    println(s"end ${Thread.currentThread.getName}")
 }
