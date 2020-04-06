a15b08a4fcd3ed9d880bd5d78c6ad12ad8ca74e3
Yohei Toriyama <kagemomiji@gmail.com>
2020-04-06 11:30:53 +0900

    初級実装

--- src/main/scala/QuadNumberPrinter.scala
+++ src/main/scala/QuadNumberPrinter.scala
@@ -1,3 +1,12 @@
 object QuadNumberPrinter extends App {
 
+    def printCounter : Unit = {
+        for(i <- 1 to 100000){ 
+            println(s"${Thread.currentThread.getName} : ${i}")
+    }}
+
+    for (i <- 1 to 4) {
+        new Thread(()=> printCounter).start()
+    }
+
 }
